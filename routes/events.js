56eafa8d0e0744cc6a8e532872bed80880e56dc5
YouSay-SD <yousay.sd@gmail.com>
2020-11-23 19:48:46 -0300

    Added Validations For Date

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,9 @@
 */ 
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/isDate');
+const { validateFields } = require('../middlewares/validateFields');
 const { validateJwt } = require('../middlewares/validateJwt');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
 
@@ -16,7 +19,16 @@ router.use(validateJwt);
 router.get('/', getEvents);
 
 // Create New Event
-router.post('/', createEvent);
+router.post(
+  '/',
+  [
+    check('title', 'The title is required').not().isEmpty(),
+    check('start', 'Initial Date is required').custom(isDate),
+    check('end', 'End Date is required').custom(isDate),
+    validateFields
+  ], 
+  createEvent
+);
 
 // Update Event
 router.put('/:id', updateEvent);
